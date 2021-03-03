875e1fbb7ad8fb3e4c99a895758e87290b5d8c73
Hieu <simshieu1@gmail.com>
2021-03-03 10:11:44 -0800

    working on addVertex

--- problems/graph.js
+++ problems/graph.js
@@ -2,14 +2,24 @@
 class Graph {
   constructor() {
     // Code goes here ...
+    this.adjList = {};
   }
 
   addVertex(vertex) {
     // Code goes here ...
+    if(!this.adjList[vertex]) {
+      this.adjList[vertex] = [];
+    }
   }
 
   addEdges(srcValue, destValue) {
     // Code goes here ...
+    //check first if adj list at src value (current node) if that has edges already
+    if(!this.adjList[srcValue]) this.adjList.addVertex(srcValue);
+    if(!this.adjList[destValue]) this.adjList.addVertex(destValue);
+
+    const src = this.adjList[srcValue];
+    const dest = this.adjList[destValue];
   }
 
   buildGraph(edges) {
